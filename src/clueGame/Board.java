67bcfd4e26489896b592bb53ed64384fc63d9cfb
Caroline Arndt <carolinearndt@mymail.mines.edu>
2019-10-15 16:25:42 -0600

    added more skeleton files

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -12,8 +12,36 @@ public class Board {
 	private Map<Character, String> legend;
 	private Map<BoardCell, Set<BoardCell>> adjMatrix;
 	private Set<BoardCell> targets;
+	private String boardConfigFile;
+	private String roomConfigFile;
+	private static Board theInstance = new Board();
+	
 	public Board() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Board getInstance() {
+		return theInstance;
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadRoomConfig() {
+		
+	}
+	
+	public void loadBoardConfig() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets(BoardCell cell, int pathLength) {
+		
 	}
 
 }
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,6 +1,25 @@
 package clueGame;
 
 public class BoardCell {
-	public int x;
-	public int y;
+	private int row;
+	private int column;
+	private Character initial;
+	
+	public BoardCell() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isWalkway() {
+		return true;
+	}
+	
+	public boolean isRoom() {
+		return true;
+	}
+	
+	public boolean isDoorway() {
+		return true;
+	}
+	
 }
