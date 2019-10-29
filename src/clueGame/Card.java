2fb339696e28fbcf4cc44ef0156067b6b52cccdf
Caroline Arndt <carolinearndt@mymail.mines.edu>
2019-10-29 13:49:19 -0600

    preparing for players

---
+++ src/clueGame/Card.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+public class Card {
+	private String cardName;
+	public Card() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+
+}
---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,21 @@
+package clueGame;
+
+import java.util.Set;
+
+public class ComputerPlayer extends Player{
+
+	public ComputerPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+	public BoardCell pickLocation(Set<BoardCell> targets) {
+		return null;
+	}
+	public void makeAccusation() {
+		
+	}
+	public void createSuggestion() {
+		
+	}
+
+	
+}
