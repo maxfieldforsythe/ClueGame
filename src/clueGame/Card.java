9bbd69f730e54a055b46c0b9fa0b45f29ae71b09
Max Forsythe <maxfieldforsythe@gmail.com>
2019-10-30 19:33:24 -0600

    Added weapon and person config files

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -2,9 +2,13 @@ package clueGame;
 
 public class Card {
 	private String cardName;
+	private CardType cardType;
+	
 	public Card() {
 		// TODO Auto-generated constructor stub
 	}
+	
+
 	@Override
 	public boolean equals(Object obj) {
 		// TODO Auto-generated method stub
